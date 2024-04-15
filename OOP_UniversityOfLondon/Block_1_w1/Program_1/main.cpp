7b6c470feea1245e743317849d663ba949077886
NSergeevich87 <niknikitaser@gmail.com>
2024-04-15 15:17:28 +0200

    WorkWithMenuSystemAndSomeFunctions

--- OOP_UniversityOfLondon/Block_1_w1/Program_1/main.cpp
+++ OOP_UniversityOfLondon/Block_1_w1/Program_1/main.cpp
@@ -1,19 +1,125 @@
 #include <iostream>
+#include <map>
 
 using namespace std;
-// Function to caluculate first 10 natural numbers and return numper and print the sum
-int calculateFirst10NaturalNumbers() {
-    int sum = 0;
-    for (int i = 1; i <= 10; i++) {
-        sum += i;
-        cout << sum << " ";
-    } 
-    return sum;
-} 
 
-// Main function
-int main() {
-    int result = calculateFirst10NaturalNumbers();
-    cout << "\nSum of first 10 natural numbers is: " << result << endl;
+void printMenu()
+{
+    cout << "====================================" << endl;
+    cout << "1: Print help" << endl;
+    cout << "2: Print exchange stats" << endl;
+    cout << "3: Make an offer" << endl;
+    cout << "4: Make a bid" << endl;
+    cout << "5: Print wallet" << endl;
+    cout << "6: Continue" << endl;
+    cout << "7: Quit" << endl;
+    cout << "====================================" << endl;
+}
+
+int getUserChoice()
+{
+    int choice;
+    cout << "Please enter a number: ";
+    cin >> choice;
+    cout << "\nYou entered: " << choice << endl;
+    return choice;
+}
+
+void printHelp()
+{
+    cout << "Help - This is a simple exchange program" << endl;
+}
+
+void printExchangeStats()
+{
+    cout << "Exchange stats - No stats available" << endl;
+}
+
+void makeAnOffer()
+{
+    cout << "Make an offer - No offer available" << endl;
+}
+
+void makeABid()
+{
+    cout << "Make a bid - No bid available" << endl;
+}
+
+void printWallet()
+{
+    cout << "Print wallet - No wallet available" << endl;
+}
+
+void goNextTimeFrame()
+{
+    cout << "Continue - Continue the program" << endl;
+}
+
+void shutDown()
+{
+    cout << "Quit - Quit the program" << endl;
+}
+
+void printInvalidChoice()
+{
+    cout << "Invalid choice" << endl;
+}
+
+bool choiceProcess(int num)
+{
+    map<int, void (*)()> choiceMap;
+    choiceMap[1] = printHelp;
+    choiceMap[2] = printExchangeStats;
+    choiceMap[3] = makeAnOffer;
+    choiceMap[4] = makeABid;
+    choiceMap[5] = printWallet;
+    choiceMap[6] = goNextTimeFrame;
+    choiceMap[7] = shutDown;
+
+    switch (num)
+    {
+    case 1:
+        choiceMap[1]();
+        break;
+    case 2:
+        choiceMap[2]();
+        break;
+    case 3:
+        choiceMap[3]();
+        break;
+    case 4:
+        choiceMap[4]();
+        break;
+    case 5:
+        choiceMap[5]();
+        break;
+    case 6:
+        choiceMap[6]();
+        break;
+    case 7:
+        choiceMap[7]();
+        return true;
+        break;
+    default:
+        printInvalidChoice();
+        break;
+    }
+
+    return false;
+}
+
+int main() 
+{
+    bool quit = false;
+
+    while (!quit)
+    {
+        printMenu();
+
+        int choice = getUserChoice();
+
+        quit = choiceProcess(choice);
+    }
+
     return 0;
 }
