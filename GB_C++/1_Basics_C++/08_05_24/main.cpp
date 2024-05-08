17dc49d1a574cbc4acf5b02d38f8180495230414
NSergeevich87 <niknikitaser@gmail.com>
2024-05-08 11:08:46 +0200

    gb_c++_hw_1

---
+++ GB_C++/1_Basics_C++/08_05_24/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() 
+{
+    cout << setfill('=') << setw(20) << '=' << endl; 
+
+    cout << "||     Hello,     ||\n||     World!     ||" << endl;
+
+    cout << setfill('=') << setw(20) << '=' << endl; 
+
+    return 0;
+}
